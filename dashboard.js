06e2973178cc76a410d7e74778cde6a39acfe804
Ben Daubin <bdaubin@tesla.com>
2026-07-01 21:42:56 +0200

    v3.3 - Add favicon (Tesla T), fix bookmarklet URL (raw.githubusercontent)

--- dashboard.js
+++ dashboard.js
@@ -34,6 +34,7 @@
 
   w.document.open();
   w.document.write('<!DOCTYPE html><html><head><meta charset="utf-8"><title>DASH - Delivery Automation Smart Hub</title>'
+  +'<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 32 32%22><rect width=%2232%22 height=%2232%22 rx=%226%22 fill=%22%23171a20%22/><text x=%2216%22 y=%2223%22 font-size=%2220%22 text-anchor=%22middle%22 fill=%22%23f4f4f5%22 font-family=%22sans-serif%22 font-weight=%22700%22>T</text></svg>" />'
   +'<style>'
   +'*{box-sizing:border-box;margin:0;padding:0}'
   +'@font-face{font-family:UST;font-weight:400;font-display:swap;src:url(https://digitalassets.tesla.com/tesla-design-system/raw/upload/static/fonts/universal-sans-2/web/text/Universal-Sans-Text-Regular.woff2) format(woff2)}'
